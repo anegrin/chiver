67d32fabbc8feef587f2c10d2c825ce3f0b985ae
alessandro negrin <alessandro.negrin@sysdig.com>
2021-03-09 12:24:16 +0100

    proper function override

--- app/src/main/java/io/github/chiver/Constants.java
+++ app/src/main/java/io/github/chiver/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     static final String TC_BASE_URL = "https://" + TC + ".com/";
     static final String TC_MAIN_FEED = TC_BASE_URL + "feed/";
     static final String TC_FEED_PATTERN = TC_BASE_URL + "feed/?paged=%s";
+    static final String REMOTE_SCRIPT_URL = "https://raw.githubusercontent.com/anegrin/chiver/next/app/src/main/assets/script.js";
     public static final String TAG = Chiver.class.getSimpleName();
 
     private Constants() {
